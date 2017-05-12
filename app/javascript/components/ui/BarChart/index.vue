abe89bb1506f1e01055f5060bebc961b7b6f2ce2
Magons <vano47@bk.ru>
2017-05-12 20:24:33 +0300

    Add tween for bar

--- app/javascript/components/ui/BarChart/index.vue
+++ app/javascript/components/ui/BarChart/index.vue
@@ -5,16 +5,46 @@
 </template>
 
 <script>
+  import TWEEN from 'tween.js'
+
   export default {
     props: ['value'],
     data () {
       return {
-
+        tweeningValue: 0
       }
     },
     computed: {
       percent () {
-        return `${this.value}%`
+        return `${this.tweeningValue}%`
+      }
+    },
+    watch: {
+      value: function (newValue, oldValue) {
+        this.tween(oldValue, newValue)
+      }
+    },
+    mounted () {
+      this.tween(0, this.value)
+    },
+    methods: {
+      tween (startValue, endValue) {
+        var vm = this
+        var animationFrame
+        function animate (time) {
+          TWEEN.update(time)
+          animationFrame = requestAnimationFrame(animate)
+        }
+        new TWEEN.Tween({ tweeningValue: startValue })
+          .to({ tweeningValue: endValue }, 500)
+          .onUpdate(function () {
+            vm.tweeningValue = this.tweeningValue.toFixed(vm.roundNumber)
+          })
+          .onComplete(function () {
+            cancelAnimationFrame(animationFrame)
+          })
+          .start()
+        animationFrame = requestAnimationFrame(animate)
       }
     }
   }
